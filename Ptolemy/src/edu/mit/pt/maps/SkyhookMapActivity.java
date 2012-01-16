de2cf43547947651121cd41c3a90dde4384df312
Saif Hakim <saif@mit.edu>
2012-01-16 18:46:01 -0500

    SkyhookMapActivity successfully determines location and logs it. Still need to show on the map. Also, rotation causes the activity to start again...

--- Ptolemy/src/edu/mit/pt/maps/SkyhookMapActivity.java
+++ Ptolemy/src/edu/mit/pt/maps/SkyhookMapActivity.java
@@ -4,27 +4,35 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.maps.MapActivity;
+import com.skyhookwireless.wps.RegistrationCallback;
 import com.skyhookwireless.wps.WPS;
 import com.skyhookwireless.wps.WPSAuthentication;
 import com.skyhookwireless.wps.WPSContinuation;
 import com.skyhookwireless.wps.WPSLocation;
 import com.skyhookwireless.wps.WPSLocationCallback;
+import com.skyhookwireless.wps.WPSPeriodicLocationCallback;
 import com.skyhookwireless.wps.WPSReturnCode;
 import com.skyhookwireless.wps.WPSStreetAddressLookup;
+import com.skyhookwireless.wps.XPS;
 
 import edu.mit.pt.Config;
 import edu.mit.pt.R;
 
 public class SkyhookMapActivity extends MapActivity {
+	final static int MAX_XPS_DELAY = 10; 
+	XPS xps;
+	WPSAuthentication auth;
 	/** Called when the activity is first created. */
+	// FIXME: onCreate is triggered by screen rotations. xps/auth needs to only happen once.
+	// See: http://stackoverflow.com/questions/456211/activity-restart-on-rotation-android/
+	//      http://developer.android.com/guide/topics/resources/runtime-changes.html#HandlingTheChange
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map_skyhook);
 		
-		Log.v(Config.TAG, "SMA: Let's authenticate!");
 		authenticateSkyhook();
-		Log.v(Config.TAG, "SMA: Ran authenticate");
+		startLocation();
 	}
 
 	@Override
@@ -35,47 +43,51 @@ public class SkyhookMapActivity extends MapActivity {
 	public void authenticateSkyhook(){
 		// Create the authentication object
 		// myAndroidContext must be a Context instance
-		WPS wps = new WPS(this);
-		WPSAuthentication auth = new WPSAuthentication(getString(R.string.skyhook_username), getString(R.string.skyhook_realm));
 		
-		// Callback object
-		WPSLocationCallback callback = new WPSLocationCallback()
-		{
-			// What the application should do after it's done
-			public void done()
-			{
-				Log.v(Config.TAG, "WPS: Donezo.");
-				// after done() returns, you can make more WPS calls.
+		// Authenticate using personal username/realm.
+		Log.v(Config.TAG+"_xps", "Trying to authenticate!");
+		auth = new WPSAuthentication(getString(R.string.skyhook_username), getString(R.string.skyhook_realm));
+		xps = new XPS(this);
+		xps.registerUser(auth, null, new RegistrationCallback(){
+			@Override
+			public void done() {
+				Log.v(Config.TAG+"_xps","Done with registration.");
 			}
-
-			// What the application should do if an error occurs
-			public WPSContinuation handleError(WPSReturnCode error)
-			{
-				handleWPSError(error); // you'll implement handleWPSError()
-
-				// To retry the location call on error use WPS_CONTINUE,
-				// otherwise return WPS_STOP
-				return WPSContinuation.WPS_STOP;
+			@Override
+			public WPSContinuation handleError(WPSReturnCode error) {
+				// Retry if auth failure
+				Log.v(Config.TAG+"_xps","Auth fail, trying again.");
+				return WPSContinuation.WPS_CONTINUE;
 			}
-
-			private void handleWPSError(WPSReturnCode error) {
-				Log.v(Config.TAG, "WPS ERROR :"+error);
+			@Override
+			public void handleSuccess() {
+				// Yay we're authed!
+				Log.v(Config.TAG+"_xps","Auth success.");
 			}
-			// Implements the actions using the location object
-			public void handleWPSLocation(WPSLocation location)
-			{
-				// you'll implement printLocation()
-				printLocation(location.getLatitude(), location.getLongitude());
-			}
-		};
-		// Call the location function with callback
-		wps.getLocation(auth,
-		                WPSStreetAddressLookup.WPS_NO_STREET_ADDRESS_LOOKUP,
-		                callback);
-		
+			
+		});
 	}
-	protected void printLocation(double latitude, double longitude) {
-		Log.v(Config.TAG, "WPS: "+latitude+", "+longitude);
+	public void startLocation(){
+		xps.getXPSLocation(auth, MAX_XPS_DELAY, XPS.EXACT_ACCURACY, new WPSPeriodicLocationCallback(){
+			@Override
+			public void done() {
+				Log.v(Config.TAG+"_xps","Done with location?");
+			}
+			@Override
+			public WPSContinuation handleError(WPSReturnCode error) {
+				Log.v(Config.TAG+"_xps","Location error... trying again.");
+				return WPSContinuation.WPS_CONTINUE;
+			}
+			@Override
+			public WPSContinuation handleWPSPeriodicLocation(WPSLocation loc) {
+				printLocation(loc.getLatitude(),loc.getLongitude(),loc.getAltitude());
+				return WPSContinuation.WPS_CONTINUE;
+			}
+			
+		});
+	}
+	protected void printLocation(double latitude, double longitude, double altitude) {
+		Log.v(Config.TAG+"_xps","LOCATION: "+latitude+", "+longitude+", "+altitude);
 	}
 	
 	
