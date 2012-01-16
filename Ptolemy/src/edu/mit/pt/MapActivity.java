e444f1c01e9c2abb39aa90b817927156483eaa99
Saif Hakim <saif@mit.edu>
2012-01-16 01:41:54 -0500

    Forgot to add new files.

---
+++ Ptolemy/src/edu/mit/pt/MapActivity.java
@@ -0,0 +1,13 @@
+package edu.mit.pt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_main);
+    }
+}
