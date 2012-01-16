f5618160ae2f80f1c663290a2befef81d3be93ea
Saif Hakim <saif@mit.edu>
2012-01-16 02:33:22 -0500

    Map shows up in PtolemyMapActivity.

--- Ptolemy/src/edu/mit/pt/PtolemyMapActivity.java
+++ Ptolemy/src/edu/mit/pt/PtolemyMapActivity.java
@@ -1,13 +1,19 @@
 package edu.mit.pt;
 
+import com.google.android.maps.MapActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MapActivity extends Activity {
+public class PtolemyMapActivity extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_main);
     }
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
 }
