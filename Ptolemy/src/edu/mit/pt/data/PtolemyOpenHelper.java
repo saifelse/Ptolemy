8761ff58ac01c2c3c6f2642b494425b1c79d9989
Joshua Ma <me@joshma.com>
2012-01-20 16:12:58 -0500

    consolidating tables

--- Ptolemy/src/edu/mit/pt/data/PtolemyOpenHelper.java
+++ Ptolemy/src/edu/mit/pt/data/PtolemyOpenHelper.java
@@ -3,25 +3,27 @@ package edu.mit.pt.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import edu.mit.pt.bookmarks.BookmarksTable;
 
-public class PlacesOpenHelper extends SQLiteOpenHelper {
-	private static final int DATABASE_VERSION = 5;
-	public static final String DATABASE_NAME = "places.db";
+public class PtolemyOpenHelper extends SQLiteOpenHelper {
+	private static final int DATABASE_VERSION = 1;
+	public static final String DATABASE_NAME = "ptolemy.db";
 	
 
-	public PlacesOpenHelper(Context context) {
+	public PtolemyOpenHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		PlacesTable.onCreate(db);
-
+		BookmarksTable.onCreate(db);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		PlacesTable.onUpgrade(db, oldVersion, newVersion);
+		BookmarksTable.onUpgrade(db, oldVersion, newVersion);
 	}
 
 }
