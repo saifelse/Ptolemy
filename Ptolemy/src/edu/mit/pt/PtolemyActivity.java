b5cbd2a41d24b2aee4bd863538ff6fad93908dd2
Thomas Georgiou <tageorgiou@gmail.com>
2012-01-20 20:09:29 -0500

    Remove unit tests and add in wifi testing

--- Ptolemy/src/edu/mit/pt/PtolemyActivity.java
+++ Ptolemy/src/edu/mit/pt/PtolemyActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import edu.mit.pt.location.WifiLocation;
 import edu.mit.pt.maps.LocationSetter;
 import edu.mit.pt.maps.PtolemyMapActivity;
 import edu.mit.pt.maps.XPSOverlay;
@@ -18,6 +19,8 @@ public class PtolemyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        WifiLocation w = new WifiLocation(this);
+        w.scanResults();
     }
     
     public void onPause(){
