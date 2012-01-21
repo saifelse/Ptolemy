fdd9a98587cf6bcac6282117ad0e2c6319ccca23
Thomas Georgiou <tageorgiou@gmail.com>
2012-01-20 20:10:54 -0500

    Add in WifiLocation.java

---
+++ Ptolemy/src/edu/mit/pt/location/WifiLocation.java
@@ -0,0 +1,23 @@
+package edu.mit.pt.location;
+
+import java.util.List;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
+public class WifiLocation {
+	WifiManager wifi;
+	public WifiLocation(Context context) {
+		wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+	}
+	
+	public void scanResults() {
+		List<ScanResult> results = wifi.getScanResults();
+		for (ScanResult r: results) {
+			System.out.println(r.BSSID);
+		}
+	}
+	
+	
+}
